dc6f2243388c1b7ec642ce56e54fe11079a76a39
wkgcass <wkgcass@hotmail.com>
2024-07-07 09:10:35 +0800

    add RuntimeResourceAccess mock

---
+++ nativeimage/src/main/java/org/graalvm/nativeimage/hosted/RuntimeResourceAccess.java
@@ -0,0 +1,10 @@
+package org.graalvm.nativeimage.hosted;
+
+public class RuntimeResourceAccess {
+    public static void addResource(Module module, String resourcePath) {
+        throw new UnsupportedOperationException();
+    }
+
+    private RuntimeResourceAccess() {
+    }
+}
