1e2390aeb3c446a74518b0aed3bf33ebbda08b40
wkgcass <wkgcass@hotmail.com>
2026-06-08 15:00:10 +0800

    add upcall in RuntimeForeignAccess

--- nativeimage/src/main/java/org/graalvm/nativeimage/hosted/RuntimeForeignAccess.java
+++ nativeimage/src/main/java/org/graalvm/nativeimage/hosted/RuntimeForeignAccess.java
@@ -1,11 +1,30 @@
 package org.graalvm.nativeimage.hosted;
 
+import java.lang.invoke.MethodHandle;
+
 public final class RuntimeForeignAccess {
     /**
      * @param desc    {@link java.lang.foreign.FunctionDescriptor}
      * @param options {@link java.lang.foreign.Linker.Option}
      */
     public static void registerForDowncall(Object desc, Object... options) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * @param desc    {@link java.lang.foreign.FunctionDescriptor}
+     * @param options {@link java.lang.foreign.Linker.Option}
+     */
+    public static void registerForUpcall(Object desc, Object... options) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * @param target  {@link MethodHandle}
+     * @param desc    {@link java.lang.foreign.FunctionDescriptor}
+     * @param options {@link java.lang.foreign.Linker.Option}
+     */
+    public static void registerForDirectUpcall(MethodHandle target, Object desc, Object... options) {
         throw new UnsupportedOperationException();
     }
 
